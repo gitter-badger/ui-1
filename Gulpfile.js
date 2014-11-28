8c941a0c1697692dbbf832fd9ff58d91947fa007
HipsterBrown <headhipster@hipsterbrown.com>
2014-11-28 16:12:52 -0500

    annotate Gulpfile for easier referencing

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,22 +6,26 @@ var rename = require('gulp-rename');
 var minifyCSS = require('gulp-minify-css');
 
 var paths = {
-  'scss': 'src/sass/**/*.scss',
+  'scss': 'src/sass/main.scss',
   'css': 'dist/**/*.css'
 };
 
+// Required vendor files for use in the static site.
 var vendors = ['bower_components/jquery/dist/jquery.min.js'];
 
+// Files paths to be used while serving the static site.
 var serve = {
   'css': 'public',
   'js': 'public',
   'vendor': 'public/vendor'
 };
 
+// Build destination
 var dest = {
   'css': 'dist/',
 };
 
+// Serves the contents of the public folder with livereload turned on.
 gulp.task('serve', function(){
   connect.server({
     root: ['public'],
@@ -29,6 +33,7 @@ gulp.task('serve', function(){
   });
 });
 
+// Compiles the main.scss file and places it in the root of the public folder
 gulp.task('styles', function(){
   return gulp.src(paths.scss)
     .pipe(sass())
@@ -38,18 +43,23 @@ gulp.task('styles', function(){
     .pipe(connect.reload());
 });
 
+// Grabs to specified bower_components files and places them in the vedor folder of the public folder
 gulp.task('vendor', function(){
   return gulp.src(vendors)
     .pipe(gulp.dest(serve.vendor))
     .pipe(connect.reload());
 });
 
+// Watches specified files for use in livereload
 gulp.task('watch', function(){
   gulp.watch([paths.scss], ['styles']);
 });
 
+// Default task, compiles styles, serves vendor, starts server, and starts watch.
 gulp.task('default', ['styles','vendor','serve', 'watch']);
 
+
+// Build task compiles styles, minimizes the file, renames it, and places it in the dist folder.
 gulp.task('build', function(){
   return gulp.src(paths.scss)
     .pipe(sass())
