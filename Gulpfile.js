739e482bcca872997e2e2adc338c0ac0397fbc8e
James Brooks <james@bluebaytravel.co.uk>
2014-12-01 09:01:46 +0000

    Let's use 4 spaces

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,12 +6,12 @@ var rename = require('gulp-rename');
 var minifyCSS = require('gulp-minify-css');
 
 var paths = {
-  'html': ['public/**/*.html'],
-  'scss': {
-    'all': ['src/sass/*.scss','src/sass/**/*.scss'],
-    'main': 'src/sass/main.scss'
-  },
-  'css': 'dist/**/*.css'
+    'html': ['public/**/*.html'],
+    'scss': {
+        'all': ['src/sass/*.scss','src/sass/**/*.scss'],
+        'main': 'src/sass/main.scss'
+    },
+    'css': 'dist/**/*.css'
 };
 
 // Required vendor files for use in the static site.
@@ -19,50 +19,50 @@ var vendors = ['bower_components/jquery/dist/jquery.min.js'];
 
 // Files paths to be used while serving the static site.
 var serve = {
-  'css': 'public',
-  'js': 'public',
-  'vendor': 'public/vendor'
+    'css': 'public',
+    'js': 'public',
+    'vendor': 'public/vendor'
 };
 
 // Build destination
 var dest = {
-  'css': 'dist/',
+    'css': 'dist/',
 };
 
 // Serves the contents of the public folder with livereload turned on.
 gulp.task('serve', function(){
-  connect.server({
-    root: ['public'],
-    livereload: true
-  });
+    connect.server({
+        root: ['public'],
+        livereload: true
+    });
 });
 
 gulp.task('html', function(){
-  return gulp.src(paths.html)
-    .pipe(connect.reload());
+    return gulp.src(paths.html)
+        .pipe(connect.reload());
 });
 
 // Compiles the main.scss file and places it in the root of the public folder
 gulp.task('styles', function(){
-  return gulp.src(paths.scss.main)
-    .pipe(sass())
-    .pipe(autoprefix())
-    .pipe(rename('cachet.css'))
-    .pipe(gulp.dest(serve.css))
-    .pipe(connect.reload());
+    return gulp.src(paths.scss.main)
+        .pipe(sass())
+        .pipe(autoprefix())
+        .pipe(rename('cachet.css'))
+        .pipe(gulp.dest(serve.css))
+        .pipe(connect.reload());
 });
 
 // Grabs to specified bower_components files and places them in the vedor folder of the public folder
 gulp.task('vendor', function(){
-  return gulp.src(vendors)
-    .pipe(gulp.dest(serve.vendor))
-    .pipe(connect.reload());
+    return gulp.src(vendors)
+        .pipe(gulp.dest(serve.vendor))
+        .pipe(connect.reload());
 });
 
 // Watches specified files for use in livereload
 gulp.task('watch', function(){
-  gulp.watch(paths.html, ['html']);
-  gulp.watch(paths.scss.all, ['styles']);
+    gulp.watch(paths.html, ['html']);
+    gulp.watch(paths.scss.all, ['styles']);
 });
 
 // Default task, compiles styles, serves vendor, starts server, and starts watch.
@@ -71,21 +71,21 @@ gulp.task('default', ['styles','vendor','serve', 'watch']);
 
 // Build task compiles styles, minimizes the file, renames it, and places it in the dist folder.
 gulp.task('build', function(){
-  return gulp.src(paths.scss)
-    .pipe(sass())
-    .pipe(autoprefix())
-    .pipe(minifyCSS({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename('cachet.min.css'))
-    .pipe(gulp.dest(dest.css));
+    return gulp.src(paths.scss)
+        .pipe(sass())
+        .pipe(autoprefix())
+        .pipe(minifyCSS({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename('cachet.min.css'))
+        .pipe(gulp.dest(dest.css));
 });
 
 gulp.task('build-grid', function(){
-  return gulp.src('bower_components/csswizardry-grids/csswizardry-grids.scss')
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
-    .pipe(rename('cachet.layout.css'))
-    .pipe(gulp.dest(dest.css));
+    return gulp.src('bower_components/csswizardry-grids/csswizardry-grids.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(rename('cachet.layout.css'))
+        .pipe(gulp.dest(dest.css));
 });
